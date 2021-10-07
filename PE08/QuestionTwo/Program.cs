2ba379a597c2d184a68ad71049c48ec98bceb63c
Rachel DuBois <rachel@duboishouse.com>
2021-10-06 21:05:28 -0400

    Commit for Unit One Exam Question Four, alongside a few older things that I forgot about.

---
+++ PE08/QuestionTwo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static bool Write()
+            {
+                Console.WriteLine("Text output from function.");
+            }
+
+            static void myFunction(string label, params int[] args, bool showLabel)
+            {
+                if (showLabel)
+                {
+                    Console.WriteLine(label);
+                }
+
+                foreach (int i in args)
+                {
+                    Console.WriteLine("{0}", i);
+                }
+            }
+
+        }
+    }
+}
